37a9078f13aef8cf548efa159917e30cf0bf64a5
Aleksei <85HopER85@gmail.com>
2020-10-04 02:40:04 +0000

    Init input file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include <fstream>
+
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+	ifstream fout;
+	fout.open("inp");
+	
+
+	fout.close();
+	return 0;
 }
