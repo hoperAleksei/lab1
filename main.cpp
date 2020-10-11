0450a9bef33f9510f596f72f41127b5c6fdeeeeb
Aleksei <85HopER85@gmail.com>
2020-10-11 06:37:13 +0000

    Added consts and started type declaration

--- main.cpp
+++ main.cpp
@@ -3,10 +3,47 @@
 
 using namespace std;
 
+/*
+* Блок описания констант
+*/
+
+#define BASE 16
+#define BLOCK_LENGTH 4
+#define LINE_COUNT 100
+#define BLOCK_COUNT 25
+
+/*
+* Блок описания типов
+*/
+
+using block = char[BLOCK_LENGTH];
+
+struct partOfNum
+{
+	unsigned char count;
+	
+};
+
+struct number
+{
+	bool sign;
+	partOfNum inter;
+	partOfNum fact;
+};
+
+
+
+
+
+
+
+
+
 int main() {
 	ifstream fout;
 	fout.open("inp");
-	
+
+	//cout << __cplusplus << endl;
 
 	fout.close();
 	return 0;
