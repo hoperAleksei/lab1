6098a1a7468b150cedce63219dc38f88230b60e9
hoperAleksei <85HopER85@gmail.com>
2020-10-18 13:15:42 +1000

    Added read and write functions Added digit functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <valarray>
 #include <cstring>
 
 using namespace std;
@@ -55,26 +54,26 @@ struct number
  *
 */
 
-
 int getDigitWeight(char CDigit)
 {
 	/*
-	 * Функция получает для каждой цифры типа char ее вес в int
+	 * Функция получает для каждой цифры типа char ее вес (значение) в int
 	*/
 	
-	// TODO
-	
+	string validDigits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	return validDigits.find(CDigit);
 }
 
 char getDigitRepr(int IDigit)
 {
 	/*
-	 *
+	 * Функция получает для каждой цифры типа int ее представление в char
 	*/
 	
-	// TODO
-	
+	string validDigits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	return validDigits[IDigit];
 }
+
 
 #pragma endregion general_functions
 
@@ -233,20 +232,172 @@ number subNum(number min, number sub)
 	
 }
 
-number readNum(string numb, bool &Error)
+#pragma region functions_for_readNum
+
+
+void errorThrow(bool &error, int len, int dot)
+{
+	error = true;
+	cout << "Error!!!\tLen: " << len << " dot pos: " << dot << endl;
+}
+
+void checkBlock(block_t block, bool &error)
+{
+	int digit;
+	for (int i = 0; i < BLOCK_LENGTH; ++i)
+	{
+		if (block[i] == '\0')
+		{
+			break;
+		}
+		digit = getDigitWeight(block[i]);
+		if ((digit == -1) || (digit >= BASE))
+		{
+			cout << digit << '=' << block[i] << endl;
+			error = true;
+		}
+	}
+}
+
+partOfNum readInter(string SInter, bool &error)
+{
+	partOfNum res;
+	
+	int len = SInter.length();
+	int lastBlockLength = len % BLOCK_LENGTH;
+	lastBlockLength = lastBlockLength == 0 ? BLOCK_LENGTH : lastBlockLength;
+	int count = len / BLOCK_LENGTH + (lastBlockLength == BLOCK_LENGTH ? 0 : 1);
+	
+	
+	
+	if (((len > 1) && (SInter[0] == '0') || (count > BLOCK_COUNT)))
+	{
+		error = true;
+	}
+	else
+	{
+		strcpy(res.blocks[count-1], SInter.substr(0, lastBlockLength).c_str());
+		checkBlock(res.blocks[count-1], error);
+	//	cout << SInter.substr(0, lastBlockLength).c_str() << ' ' << count << endl;
+		for(int i = count-2; i >=0; --i)
+		{
+			//cout << i;
+			strcpy(res.blocks[i], SInter.substr(lastBlockLength+(count-2-i)*BLOCK_LENGTH, BLOCK_LENGTH).c_str());
+			checkBlock(res.blocks[i], error);
+		}
+		res.count = count;
+	}
+/*	for (int i = res.count-1; i >= 0 ; --i)
+	{
+		cout << i <<'(' << res.blocks[i] << ')';
+	}
+	cout << endl;*/
+	
+	return res;
+}
+
+partOfNum readFact(string SFact, bool &error)
+{
+	partOfNum res;
+	
+	int len = SFact.length();
+	int lastBlockLength = len % BLOCK_LENGTH;
+	lastBlockLength = lastBlockLength == 0 ? BLOCK_LENGTH : lastBlockLength;
+	int count = len / BLOCK_LENGTH + (lastBlockLength == BLOCK_LENGTH ? 0 : 1);
+	
+	if (((len > 1) && (SFact[len-1] == '0') || (count > BLOCK_COUNT)))
+	{
+		error = true;
+	}
+	else
+	{
+	//	cout << "------===" << count << endl;
+		for(int i = count-1; i > 0; --i)
+		{
+			strcpy(res.blocks[i], SFact.substr((count-1-i)*BLOCK_LENGTH, BLOCK_LENGTH).c_str());
+			checkBlock(res.blocks[i], error);
+		}
+		strcpy(res.blocks[0], SFact.substr(len-lastBlockLength).c_str());
+		checkBlock(res.blocks[0], error);
+		res.count = count;
+	}
+	/*for (int i = res.count-1; i >= 0 ; --i)
+	{
+		cout << i <<'(' << res.blocks[i] << ')';
+	}
+	cout << endl;*/
+	return res;
+}
+
+
+#pragma endregion functions_for_readNum
+
+number readNum(string numb, bool &error)
 {
 	/*
 	 * Функция считывания числа
 	*/
-	//if ()
+	
+	number res;
 	
 	
+	string SInter;
+	string SFact;
+	
+	int len = numb.length();
+	
+	int dot = numb.find('.');
 	
 	
+	if ((len == 0) || (dot == -1) || (dot == 0) || (dot+1 == len))
+	{
+		errorThrow(error, len, dot);
+	}
+	else
+	{
+		if (numb[0] == '-')
+		{
+			if (numb[1] == '.')
+			{
+				errorThrow(error, len, dot);
+			}
+			else
+			{
+				SInter = numb.substr(1, dot);
+				res.sign = true;
+			}
+		}
+		else
+		{
+			res.sign = false;
+			SInter = numb.substr(0, dot);
+		}
+		if (!error)
+		{
+			SFact = numb.substr(dot + 1);
+			//cout << SInter << '|' << SFact << endl;
+			
+			res.inter = readInter(SInter, error);
+			if (!error)
+			{
+				res.fact = readFact(SFact, error);
+				if (error)
+				{
+					errorThrow(error, len, dot);
+				}
+			}
+			else
+			{
+				errorThrow(error, len, dot);
+			}
+			
+			return res;
+		}
+	}
 	// TODO
 }
 
-number printNum(number numb)
+void printNum(number numb)
 {
 	/*
 	 * Функция вывода числа
@@ -254,20 +405,21 @@ number printNum(number numb)
 	
 	cout << (numb.sign ? '-' : '+');
 	// cout << numb.inter.count << '/';
-	for (int i = numb.inter.count; i >= 0 ; --i)
+	for (int i = numb.inter.count-1; i >= 0 ; --i)
 	{
-		cout << numb.inter.blocks[i];
+		cout << '(' << numb.inter.blocks[i] << ')';
 	}
 	cout << '.';
 	// cout << numb.fact.count << '/';
-	for (int i = numb.fact.count; i >= 0 ; --i)
+	for (int i = numb.fact.count-1; i >= 0 ; --i)
 	{
-		cout << numb.fact.blocks[i];
+		cout << '(' << numb.fact.blocks[i] << ')';
 	}
 	cout  << endl;
 	
-	//TODO не зависит от count
+	//TODO убрать дебаг
 }
+
 
 #pragma endregion number_functions
 
@@ -278,38 +430,37 @@ int main() {
 	
 	string line;
 	
+	bool error = false;
+	
+	number n;
 	
 	cout << "Ваша версия страндарта языка: " << __cplusplus << endl;
 	
-
-	getline(inpFile, line, '\n');
+	while (! inpFile.eof())
+	{
+		error = false;
+		getline(inpFile, line, '\n');
+		n = readNum(line, error);
+		if (!error)
+		{
+			printNum(n);
+		}
+		else
+		{
+			cout << "ERROR!!!" << line << endl;
+		}
+	}
 	
-	
-	
-	
-	/*using block_t = char[BLOCK_LENGTH];
-
-struct partOfNum
-{
-	unsigned char count;
-	block_t blocks[BLOCK_COUNT];
-};
-
-struct number
-{
-	bool sign;
-	partOfNum inter;
-	partOfNum fact;
-};*/
-	
-	number n = {true, {2, {"5678", "1234", "1234"}}, {2, {"4567", "0123"}}};
+	//number n = {true, {2, {"5678", "1234", "1234"}}, {2, {"4567", "0123"}}};
 	
 /*
 	n.inter.count = 1;
 	strcpy(n.inter.blocks[0], "1234");
 */
 	
-	printNum(n);
+	//printNum(n);
+	
+	
 	
 	inpFile.close();
 	return 0;
